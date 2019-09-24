b8394e6f5b262ecbeb845e475c3563eba01582b1
Tom <tom.grotze@kmpc.de>
2019-09-24 16:22:37 +0200

    Add config_ABB

---
+++ src/config_ABB.h
@@ -0,0 +1,40 @@
+// --------------------- Sensor Settings
+
+const char* MQTT_LOCATION = "ber";
+const char* PLANT = "ABB";
+const char* SKETCH_VERSION = "15";
+
+#define MACHINE_PIN  3 //SW02 = 5 //SW01 = 13 //LF = 3
+
+//Reboot Settings
+const int rebootHours = 24; //Reboot after X hours
+const int thresholdStoppedMinutes = 10; //When for X minutes no pulses detected
+
+// --------------------- Network Settings
+
+#define WIFI_SSID "FERTIGUNG"
+#define WIFI_PASSWORD "***REMOVED***"
+
+bool IPStatic = false;
+IPAddress staticIP(10,49,13,103); //ESP static ip
+IPAddress gateway(10,49,13,1);   //IP Address of your WiFi Router (Gateway)
+IPAddress subnet(255, 255, 255, 0);  //Subnet mask
+IPAddress dns(10,49,13,1);  //DNS
+
+#define updateServer "188.40.218.84"
+
+
+// --------------------- MQTT Settings
+
+#define MQTT_HOST IPAddress(0,0,0,0)
+#define MQTT_PORT 1883
+#define MQTT_SECURE true
+#define MQTT_USERNAME "ber"
+#define MQTT_PASSWORD "***REMOVED***"
+
+
+// --------------------- Function Settings
+
+bool usePing = true;
+bool useNTP = false; //Not in use
+bool useOTAonStart = false;
